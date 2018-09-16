825bcdce7b720a0b495c37a45c0103b45757f5e9
haspadarZ-padMensku <gonchds@gmail.com>
2018-09-16 14:50:46 +0300

    implement getZerosCount func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  // your implementation    
+  let counter = 0;
+
+  while (number >= 5) {
+    number = Math.floor(number / 5);
+    counter += number;
+  }
+
+  return counter;
 }
